bc9b2a07c40b9a2452abdb17add46745302540bc
Andika <litageming@gmail.com>
2023-01-10 11:47:27 +0700

    sd

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["programmer", "gaming", "siswa", "LIFE"];
+ const textArray = ["programmer", "gaming", "siswa", "gabut"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 1000; // Delay between current and next text
