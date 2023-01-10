617e12244725216721125d042fe3aea0640e9ac8
Andika <litageming@gmail.com>
2023-01-10 11:46:04 +0700

    fd

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cursorSpan = document.querySelector(".cursor");
 const textArray = ["programmer", "gaming", "siswa", "LIFE"];
 const typingDelay = 200;
 const erasingDelay = 100;
-const newTextDelay = 2000; // Delay between current and next text
+const newTextDelay = 1000; // Delay between current and next text
 let textArrayIndex = 0;
 let charIndex = 0;
 
