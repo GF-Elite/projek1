b411d69e18d190e90a94552b6d866d2fd8c63344
Andika <litageming@gmail.com>
2023-01-10 16:44:28 +0700

    re

--- app.js
+++ app.js
@@ -48,3 +48,20 @@ document.addEventListener("click",function (e){
       myModal.show();
   }
 })
+
+var isNS = (navigator.appName == &quot;Netscape&quot;) ? 1 : 0;
+if(navigator.appName == &quot;Netscape&quot;) document.captureEvents(Event.MOUSEDOWN||Event.MOUSEUP);
+function mischandler(){
+return false;
+}
+function mousehandler(e){
+var myevent = (isNS) ? e : event;
+var eventbutton = (isNS) ? myevent.which : myevent.button;
+if((eventbutton==1)||(eventbutton==2)||(eventbutton==3)) {
+ alert("copyThieft");
+ return false;
+ }
+}
+document.oncontextmenu = mischandler;
+document.onmousedown = mousehandler;
+document.onmouseup = mousehandler;
